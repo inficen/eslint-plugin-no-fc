85dcdd77bca1231b6fb852e4ce56d384fbce3fa1
Lionel Tay <lionel.lt.tay@gmail.com>
2022-09-15 23:54:11 +1000

    fix: update rule doc url

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,5 @@ import { ESLintUtils } from "@typescript-eslint/utils"
 
 export const ruleCreator = ESLintUtils.RuleCreator(
   (name) =>
-    `https://github.com/inficen/eslint-plugin-prefer-types/blob/main/docs/rules/${name}.md`
+    `https://github.com/inficen/eslint-plugin-no-fc/blob/main/docs/rules/${name}.md`
 )
