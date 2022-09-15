82d7d6d4ec4dbff800087bd342283542e27b9d4d
Lionel Tay <lionel.lt.tay@gmail.com>
2022-09-15 23:50:37 +1000

    fix: update rule docs link

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
-import { ESLintUtils } from "@typescript-eslint/utils";
+import { ESLintUtils } from "@typescript-eslint/utils"
 
 export const ruleCreator = ESLintUtils.RuleCreator(
   (name) =>
-    `https://github.com/inficen/eslint-plugin-no-fc/docs/rules/${name}`
-);
+    `https://github.com/inficen/eslint-plugin-prefer-types/blob/main/docs/rules/${name}.md`
+)
